1a8957acd6e6caa4f82fbe8e01cbe0edd3b7f852
riizu <rgbatty@outlook.com>
2016-07-27 00:19:59 -0600

    Adjust status endpoint to pull the newest entry

--- app/controllers/api/v1/status_controller.rb
+++ app/controllers/api/v1/status_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::StatusController < ApiBaseController
   def index
-    @version = Status.first
+    @version = Status.order(created_at: :desc).first
     render json: @version
   end
 end
--- spec/requests/api/v1/status_spec.rb
+++ spec/requests/api/v1/status_spec.rb
@@ -14,4 +14,17 @@ describe "Status Endpoint" do
     expect(parsed_status["version"]).to eq "0.0.1"
     expect(parsed_status["last_update"]).to eq formatted_created_at
   end
+
+  it "pulls the newest status information" do
+    status = create(:status, version: "0.0.1", created_at: "2012-03-27T14:53:59.000Z")
+    newer_status = create(:status, version: "0.0.2")
+
+    get "/api/v1/status"
+
+    expect(response).to be_success
+
+    parsed_status = JSON.parse(response.body)
+
+    expect(parsed_status["version"]).to eq "0.0.2"
+  end
 end
