69003e7891ae8b52a01b4f2a239920b4e4ce1ddc
riizu <rgbatty@outlook.com>
2016-07-26 23:55:12 -0600

    Add spec for status endpoint

---
+++ spec/requests/api/v1/status_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "Status Endpoint" do
+  it "Sends status information for the server" do
+    status = create(:status, version: "0.0.1")
+
+    get "/api/v1/status"
+
+    expect(response).to be_success
+
+    parsed_status = JSON.parse(response.body)
+
+    expect(parsed_status.version).to eq "0.0.1"
+    expect(parsed_status.last_update).to eq status.created_at
+  end
+end
