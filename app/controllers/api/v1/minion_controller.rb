beafce169ab3bd3e2d61d396b696cbb773a62a9d
riizu <rgbatty@outlook.com>
2016-08-01 20:00:36 -0600

    Adjust create_minion endpoint to set user status to active after creation

--- app/controllers/api/v1/minion_controller.rb
+++ app/controllers/api/v1/minion_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::MinionController < SecureApiBaseController
   end
 
   def create
+    current_user.update_attributes(status: :active)
     render json: Minion.create(name: params[:name], user: current_user)
   end
 end
